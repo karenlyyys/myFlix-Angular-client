05a43726232a34c9ed0160ce314f4b0b7f3f3f5e
karenlyyys <99241654+karenlyyys@users.noreply.github.com>
2022-12-21 15:17:14 -0800

    6.4 halfway

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -1,10 +1,100 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+import { FetchApiDataService } from '../fetch-api-data.service';
+
 
 @Component({
   selector: 'app-user-profile',
   templateUrl: './user-profile.component.html',
   styleUrls: ['./user-profile.component.scss']
 })
-export class UserProfileComponent {
 
-}
+export class UserProfileComponent implements OnInit {
+    user: any = {};
+    initialInput: any = {};
+    @Input() updatedUser = {
+      Username: '',
+      Password: '',
+      Email: '',
+      Birthday: '',
+    };
+  
+    constructor(
+      public fetchApiData: FetchApiDataService,
+      public dialogRef: MatDialogRef< UserProfileComponent >,
+      public snackBar: MatSnackBar,
+      public router: Router
+    ) {}
+  
+    ngOnInit(): void {
+      this.getUserInfo();
+    }
+  
+    /**
+     * Fetch user data via API
+     * @returns object with user information
+     * @function getUserInfo
+     */
+  
+    getUserInfo(): void {
+      this.fetchApiData.getUser().subscribe((resp: any) => {
+        this.user = resp;
+        console.log(this.user);
+        this.updatedUser.Username = this.user.Username;
+        this.updatedUser.Email = this.user.Email;
+        this.updatedUser.Birthday = this.user.Birthday;
+        console.log(this.updatedUser);
+        return this.user;
+      });
+    }
+  
+    /**
+     * Update user data, such as username, password, email, or birthday
+     * @function updateUserInfo
+     */
+  
+    updateUserInfo(): void {
+      this.fetchApiData.editUser(this.updatedUser).subscribe((result) => {
+        console.log(result);
+        this.snackBar.open('User profile successfully updated', 'OK', {
+          duration: 2000,
+        });
+        if (this.user.Username !== result.Username) {
+          localStorage.clear();
+          this.router.navigate(['welcome']);
+          this.snackBar.open(
+            'User profile successfully updated. Please login using your new credentials',
+            'OK',
+            {
+              duration: 2000,
+            }
+          );
+        }
+      });
+    }
+  
+    /**
+     * Delete user data for the user that is logged in
+     * @function deleteAccount
+     */
+  
+    deleteAccount(): void {
+      if (confirm('All your data will be lost - this cannnot be undone!')) {
+        this.router.navigate(['welcome']).then(() => {
+          this.snackBar.open(
+            'You have successfully deleted your account - we are sorry to see you go!',
+            'OK',
+            {
+              duration: 2000,
+            }
+          );
+        });
+        this.fetchApiData.deleteUser().subscribe((result) => {
+          console.log(result);
+          localStorage.clear();
+        });
+      }
+    }
+  }
